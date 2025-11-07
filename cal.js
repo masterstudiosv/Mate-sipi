6a5a7577a18eb67c13dcdca54ca9dd1da4f7cc6a
masterstudiosv <masterstudiosv@gmail.com>
2025-11-06 23:26:55 -0600

    Update print statement from 'Hello' to 'Goodbye'

--- cal.js
+++ cal.js
@@ -992,3 +992,8 @@ function cerrarSesion() {
         window.location.href = "index.html"; // Redirige al index.html
 
     }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(() => console.log("✅ Service Worker registrado"))
+      .catch(err => console.error("❌ Error al registrar SW:", err));
+  }
